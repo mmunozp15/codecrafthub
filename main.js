03629bb6276bb71e4f52afb94d1020318067eef0
mmunozp15 <memunozp15@gmail.com>
2026-09-03 20:06:44 -0500

    Add frontend interface generated by Bolt

---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <iframe src="/lms.html" title="CodeCraftHub Learning Management Platform"
+    style="position:fixed; inset:0; width:100%; height:100%; border:0;"></iframe>
+`
